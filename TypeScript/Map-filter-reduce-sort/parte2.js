eaa9161159dd3c17b20f3b26093ab6a80d1c4174
Yuri Christian <yuri-tech@outlook.com>
2021-01-09 14:30:35 -0300

    Revisão das funções Map, Filter, Reduce e Sort

---
+++ TypeScript/Map-filter-reduce-sort/parte2.js
@@ -0,0 +1,93 @@
+"use strict";
+exports.__esModule = true;
+var gameList = [
+    {
+        "id": 1,
+        "title": "The Witcher 3",
+        "platform": "XBOX"
+    },
+    {
+        "id": 2,
+        "title": "The Witcher 3",
+        "platform": "PLAYSTATION"
+    },
+    {
+        "id": 3,
+        "title": "Overwatch",
+        "platform": "PC"
+    }
+];
+var recordItemList = [
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "PLAYSTATION",
+        "genreName": "RPG"
+    },
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "XBOX",
+        "genreName": "RPG"
+    },
+    {
+        "gameTitle": "Overwatch",
+        "gamePlatform": "PC",
+        "genreName": "Shooter"
+    },
+    {
+        "gameTitle": "Overwatch",
+        "gamePlatform": "PC",
+        "genreName": "Shooter"
+    },
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "PLAYSTATION",
+        "genreName": "RPG"
+    },
+];
+var buildBarSeries = function (games, records) {
+    var mappedGames = games.map(function (game) {
+        var filteredGames = records.filter(function (record) {
+            return record.gameTitle === game.title && record.gamePlatform === game.platform;
+        });
+        return {
+            x: game.title + " | " + game.platform,
+            y: filteredGames.length
+        };
+    });
+    var sortedGames = mappedGames.sort(function (a, b) { return b.y - a.y; });
+    return sortedGames.slice(0, 8);
+};
+var getPlatformChartData = function (redord) {
+    var platforms = ['PC', 'PLAYSTATION', 'XBOX'];
+    var series = platforms.map(function (platform) {
+        var filteredGames = redord.filter(function (record) {
+            return record.gamePlatform === platform;
+        });
+        return filteredGames.length;
+    });
+    return {
+        labels: platforms,
+        series: series
+    };
+};
+var getGenreCharData = function (records) {
+    var computeRecordItem = function (obj, record) {
+        if (obj[record.genreName] !== undefined) {
+            obj[record.genreName] += 1;
+        }
+        else {
+            obj[record.genreName] = 1;
+        }
+        return obj;
+    };
+    var genreByAmount = records.reduce(computeRecordItem, {});
+    var labels = Object.keys(genreByAmount);
+    var series = labels.map(function (x) { return genreByAmount[x]; });
+    return {
+        labels: labels,
+        series: series
+    };
+};
+//console.log(getPlatformChartData(recordItemList));
+//console.log(buildBarSeries(gameList, recordItemList));
+//console.log(getGenreCharData(recordItemList));
---
+++ TypeScript/Map-filter-reduce-sort/parte2.ts
@@ -0,0 +1,131 @@
+export { }
+
+type Platform = 'XBOX' | 'PC' | 'PLAYSTATION';
+
+type RecordItem = {
+    gameTitle: string;
+    gamePlatform: Platform,
+    genreName: string;
+}
+
+type Game = {
+    id: number;
+    title: string;
+    platform: Platform;
+}
+
+const gameList : Game[] = [
+    {
+        "id": 1,
+        "title": "The Witcher 3",
+        "platform": "XBOX"
+    },
+    {
+        "id": 2,
+        "title": "The Witcher 3",
+        "platform": "PLAYSTATION"
+    },
+    {
+        "id": 3,
+        "title": "Overwatch",
+        "platform": "PC"
+    }
+]
+
+const recordItemList : RecordItem[] = [
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "PLAYSTATION",
+        "genreName": "RPG"
+    },
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "XBOX",
+        "genreName": "RPG"
+    },
+    {
+        "gameTitle": "Overwatch",
+        "gamePlatform": "PC",
+        "genreName": "Shooter"
+    },
+    {
+        "gameTitle": "Overwatch",
+        "gamePlatform": "PC",
+        "genreName": "Shooter"
+    },
+    {
+        "gameTitle": "The Witcher 3",
+        "gamePlatform": "PLAYSTATION",
+        "genreName": "RPG"
+    },
+]
+
+
+const buildBarSeries = (games: Game[], records: RecordItem[]) => {
+
+    const mappedGames = games.map(game => {
+
+        const filteredGames = records.filter(record => {
+            return record.gameTitle === game.title && record.gamePlatform === game.platform;
+        })
+
+        return {
+            x: `${game.title} | ${game.platform}`,
+            y: filteredGames.length
+        }
+    });
+
+    const sortedGames = mappedGames.sort((a, b) => b.y - a.y)
+
+    return sortedGames.slice(0, 8);
+
+}
+
+const getPlatformChartData = (redord: RecordItem[]) => {
+
+    const platforms = ['PC', 'PLAYSTATION', 'XBOX'];
+
+    const series = platforms.map(platform => {
+
+        const filteredGames = redord.filter(record => {
+            return record.gamePlatform === platform;
+        });
+        
+        return filteredGames.length;
+    })
+
+    
+    return {
+        labels: platforms,
+        series: series
+    }
+}
+
+const getGenreCharData = (records: RecordItem[]) => {
+
+    const computeRecordItem = (obj, record) => {
+
+        if(obj[record.genreName] !== undefined){
+            obj[record.genreName] += 1;
+        }else{
+            obj[record.genreName] = 1;
+        }
+        return obj;
+
+    }
+
+    const genreByAmount = records.reduce(computeRecordItem, {});    
+
+    const labels = Object.keys(genreByAmount);
+    const series = labels.map(x => genreByAmount[x]);
+
+    return {
+        labels,
+        series
+    }
+}
+
+
+//console.log(getPlatformChartData(recordItemList));
+//console.log(buildBarSeries(gameList, recordItemList));
+//console.log(getGenreCharData(recordItemList));
